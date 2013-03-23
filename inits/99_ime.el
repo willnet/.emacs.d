1f7ded74bb4c9051c5441289172efb2ba977349b
willnet <netwillnet@gmail.com>
2013-03-23 14:47:35 +0900

    Update for ATOK ver25

--- inits/99_ime.el
+++ inits/99_ime.el
@@ -1,6 +1,6 @@
 ;; http://www.oreradio.com/2012/01/10.php
 (mac-input-method-mode)
 (setq default-input-method "MacOSX")
-(mac-set-input-method-parameter "com.justsystems.inputmethod.atok24.Japanese" `title "漢")
-(mac-set-input-method-parameter "com.justsystems.inputmethod.atok24.Japanese" `cursor-type 'box)
-(mac-set-input-method-parameter "com.justsystems.inputmethod.atok24.Japanese" `cursor-color "magenta")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok25.Japanese" `title "漢")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok25.Japanese" `cursor-type 'box)
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok25.Japanese" `cursor-color "magenta")
