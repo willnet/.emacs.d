27c600bf0d7f8f29ae9644757c0a87273451e1fd
willnet <netwillnet@gmail.com>
2013-04-09 21:33:09 +0900

    Turn off auto-install-update-emacswiki-package-name
    because auto-install-update-emacswiki-package-name tries to connect emacs-wiki and launching emacs time is sometimes very slow

--- inits/30_auto-install.el
+++ inits/30_auto-install.el
@@ -3,6 +3,6 @@
 ;; インストールディレクトリの指定
 (setq auto-install-directory "~/.emacs.d/auto-install/")
 ;; EmacsWiki に登録されている elisp の名前を取得する
-(auto-install-update-emacswiki-package-name t)
+;; (auto-install-update-emacswiki-package-name t)
 ;; install-elisp の関数を利用可能にする
 (auto-install-compatibility-setup)
