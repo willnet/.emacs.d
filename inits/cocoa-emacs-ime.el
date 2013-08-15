1b235cce3bf84a2a05a0c995ef050fa5fac16a1a
willnet <netwillnet@gmail.com>
2013-08-15 16:58:19 +0900

    ATOK 2013 に対応

--- inits/cocoa-emacs-ime.el
+++ inits/cocoa-emacs-ime.el
@@ -1,6 +1,6 @@
 ;; http://www.oreradio.com/2012/01/10.php
 (mac-input-method-mode)
 (setq default-input-method "MacOSX")
-(mac-set-input-method-parameter "com.justsystems.inputmethod.atok25.Japanese" `title "漢")
-(mac-set-input-method-parameter "com.justsystems.inputmethod.atok25.Japanese" `cursor-type 'box)
-(mac-set-input-method-parameter "com.justsystems.inputmethod.atok25.Japanese" `cursor-color "magenta")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok26.Japanese" `title "漢")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok26.Japanese" `cursor-type 'box)
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok26.Japanese" `cursor-color "magenta")
