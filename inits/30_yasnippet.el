70f1c9371bad9e5a106950a990884075b711de78
willnet <netwillnet@gmail.com>
2014-06-23 14:59:30 +0900

    Add snippets path to yasnippet to go with yasnippet's update

--- inits/30_yasnippet.el
+++ inits/30_yasnippet.el
@@ -1,6 +1,8 @@
 ;; yasnippet
 (require 'yasnippet)
 (require 'dropdown-list)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"))
 (yas-global-mode 1)
 (setq yas-trigger-key "C-i")
 (setq yas-prompt-functions '(yas-dropdown-prompt
