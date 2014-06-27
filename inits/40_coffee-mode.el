1ddf314e596812b7c5d514b8722b14d14f385703
willnet <netwillnet@gmail.com>
2014-06-27 15:01:20 +0900

    Catch up with new coffee-mode's indent setting way

--- inits/40_coffee-mode.el
+++ inits/40_coffee-mode.el
@@ -1,13 +1,8 @@
-;; git clone git://github.com/defunkt/coffee-mode.git
 (require 'coffee-mode)
 
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-(defun coffee-custom ()
-  "coffee-mode-hook"
- (set (make-local-variable 'tab-width) 2))
-(add-hook 'coffee-mode-hook
-  '(lambda() (coffee-custom)))
+(custom-set-variables '(coffee-tab-width 2))
 
 ;; flymake - http://d.hatena.ne.jp/antipop/20110508/1304838383
 ;; (setq flymake-coffeescript-err-line-patterns
