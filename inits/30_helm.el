c5ecbac378eb003e0990a3e8b6ceda4d6d8f6a88
willnet <netwillnet@gmail.com>
2014-07-03 13:54:11 +0900

    Add buffer list to helm-source

--- inits/30_helm.el
+++ inits/30_helm.el
@@ -1,5 +1,6 @@
 (require 'helm-config)
 
 (setq helm-mini-default-sources '(helm-source-recentf
+                                  helm-source-buffers-list
                                   helm-source-ls-git
                                   helm-source-bookmarks))
