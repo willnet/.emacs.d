06bbac2878764d1753b64871dd1eee1b81f5ad4f
willnet <netwillnet@gmail.com>
2014-09-08 10:08:31 +0900

    Changed settings before I know but it works

--- init.el
+++ init.el
@@ -21,10 +21,17 @@
  ;; If there is more than one, they won't work right.
  '(background-color "#042028")
  '(background-mode dark)
+ '(coffee-tab-width 2)
  '(cursor-color "#708183")
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(foreground-color "#708183")
  '(haskell-notify-p t)
  '(haskell-process-type (quote cabal-dev))
  '(haskell-stylish-on-save t)
  '(haskell-tags-on-save t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "gray20" :underline t)))))
