2c9e09f7c7d6327c869058d53ab9f757af2a5ea0
willnet <netwillnet@gmail.com>
2014-10-03 14:01:01 +0900

    Chagne order of helm sources

--- inits/30_helm.el
+++ inits/30_helm.el
@@ -1,8 +1,8 @@
 (require 'helm-config)
 
-(setq helm-mini-default-sources '(helm-source-recentf
+(setq helm-mini-default-sources '(helm-source-ls-git
+                                  helm-source-recentf
                                   helm-source-buffers-list
-                                  helm-source-ls-git
                                   helm-source-bookmarks))
 
 ;; helm-ag
