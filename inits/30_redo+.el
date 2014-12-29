ef7d7b5b229c70ec86ddcd605b81eb8b86b009a8
willnet <netwillnet@gmail.com>
2014-12-29 23:26:21 +0900

    Change redo keybind to C-M-/

--- inits/30_redo+.el
+++ inits/30_redo+.el
@@ -1,4 +1,2 @@
-;; (install-elisp "http://www.emacswiki.org/emacs/download/redo+.el")
-(when (require 'redo+ nil t)
-  ;; global-map
-  (global-set-key (kbd "C-'") 'redo))
+(require 'redo+)
+(global-set-key (kbd "C-M-/") 'redo)
