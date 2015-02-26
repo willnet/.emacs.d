aa361f55ad80ee3db827719aca36b5dc8ff3ca35
willnet <netwillnet@gmail.com>
2015-02-26 11:13:28 +0900

    Change keybinds of yasnippet for using autocomplete and yasnippet

--- inits/30_yasnippet.el
+++ inits/30_yasnippet.el
@@ -4,7 +4,11 @@
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"))
 (yas-global-mode 1)
-(setq yas-trigger-key "C-i")
+
+
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "M-i") 'yas-expand)
+(define-key yas-keymap (kbd "M-i") 'yas-next-field-or-maybe-expand)
 (setq yas-prompt-functions '(yas-dropdown-prompt
                              yas-ido-prompt
                              yas-completing-prompt))
